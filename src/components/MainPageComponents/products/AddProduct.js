a17bbb4a4e0019b81c9f013c6cf3b7c8708e72df
Marko <61353801+markosolopenko@users.noreply.github.com>
2021-01-11 12:38:38 +0200

    Update AddProduct.js

--- src/components/MainPageComponents/products/AddProduct.js
+++ src/components/MainPageComponents/products/AddProduct.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { ReactComponent as CartSvg}  from '../../../assets/cart.svg'
-
+import 
 
 const AddProduct = ({product, addToCart, addProductToDetails}) => {
     return (
